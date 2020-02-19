8e4e8ef6ed91742afa553694e90879b327a295e6
TobiasKuch <kuch@hm.edu>
2020-02-19 22:28:46 +0100

    lol

---
+++ src/components/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+    <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+</template>
+
+<script>
+    import FullCalendar from '@fullcalendar/vue'
+    import dayGridPlugin from '@fullcalendar/daygrid'
+
+    export default {
+        components: {
+            FullCalendar // make the <FullCalendar> tag available
+        },
+        data() {
+            return {
+                calendarPlugins: [ dayGridPlugin ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
